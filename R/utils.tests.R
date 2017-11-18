5ef893bc87483267ef0edf2e3b5198cb93ed3cf0
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2017-11-18 14:09:38 +0200

    small cosmetic changes

--- R/utils.tests.R
+++ R/utils.tests.R
@@ -7,24 +7,24 @@ do_ttest <- function(mx, mu, stder, alt, df, conf) {
 
   inds <- alt=="less"
   if(any(inds)) {
-    res[inds,2]  <- pt(res[inds,1], df[inds])
-    res[inds,3]  <- rep(-Inf, sum(inds))
-    res[inds,4]  <- res[inds,1] + qt(conf[inds], df[inds])
+    res[inds,2] <- pt(res[inds,1], df[inds])
+    res[inds,3] <- rep(-Inf, sum(inds))
+    res[inds,4] <- res[inds,1] + qt(conf[inds], df[inds])
   }
 
   inds <- alt=="greater"
   if(any(inds)) {
-    res[inds,2]  <- pt(res[inds,1], df[inds], lower.tail=FALSE)
-    res[inds,3]  <- res[inds,1] - qt(conf[inds], df[inds])
-    res[inds,4]  <- rep(Inf, sum(inds))
+    res[inds,2] <- pt(res[inds,1], df[inds], lower.tail=FALSE)
+    res[inds,3] <- res[inds,1] - qt(conf[inds], df[inds])
+    res[inds,4] <- rep(Inf, sum(inds))
   }
 
   inds <- alt=="two.sided"
   if(any(inds)) {
-    res[inds,2]  <- 2 * pt(-abs(res[inds,1]), df[inds])
-    intrange     <- qt(1 - (1-conf[inds])/2, df[inds])
-    res[inds,3]  <- res[inds,1] - intrange
-    res[inds,4]  <- res[inds,1] + intrange
+    res[inds,2] <- 2 * pt(-abs(res[inds,1]), df[inds])
+    intrange    <- qt(1 - (1-conf[inds])/2, df[inds])
+    res[inds,3] <- res[inds,1] - intrange
+    res[inds,4] <- res[inds,1] + intrange
   }
 
   res[,3:4] <- mu + res[,3:4] * stder
