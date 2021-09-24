dde05b365a74810c4164131dd35c07a437d278cf
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2021-09-24 19:09:48 +0300

    further improvements to wilcox test speed

--- R/utils.general.R
+++ R/utils.general.R
@@ -9,27 +9,15 @@ rowVars <- function(x, n=NULL, m=NULL, na.rm=FALSE) {
   res
 }
 
-# NOTE: assumes that the input is a matrix with ranks in each row
-#       the behaviour of this function is quite trikcy
-#       it converts the matrix of ranks into an index-form representation of a different matrix,
-#       where rows represent the original rows but columns correspond to ranks (values of r).
-#       then it counts the occurance of each such index to get table of ranks for each row
 rowRankTies <- function(r) {
-  dupRows <- apply(r, 1, anyDuplicated, incomparables=NA) != 0
-  if (any(dupRows)) {
-    r <- r[dupRows,, drop=FALSE]
+  if(storage.mode(r) != "integer") {
     storage.mode(r) <- "integer"
-
-    inds  <- nrow(r) * (r - 1) + row(r)
-    msize <- nrow(r) * (ncol(r) - 1) + nrow(r)
-    t <- tabulate(inds, msize)
-
-    res <- matrix(1L, nrow=length(dupRows), ncol=ncol(r))
-    res[dupRows,][1:msize] <- t
-  } else {
-    res <- matrix(1L, nrow=length(dupRows), ncol=1)
   }
-  res
+  t <- apply(r, 1, tabulate, ncol(r), simplify=FALSE)
+  if(!is.list(t)) {
+    t <- list(t)
+  }
+  do.call(rbind, t)
 }
 
 showWarning <- function(isWarning, err) {
